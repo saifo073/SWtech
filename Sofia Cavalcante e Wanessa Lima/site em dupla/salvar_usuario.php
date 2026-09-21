34360adabcebfb5676425bc106fa19182fa08f13
Sofia Cavalcante <sofiacavalcante011@gmail.com>
2026-09-21 08:35:10 -0300

    Update salvar_usuario.php

--- Sofia Cavalcante e Wanessa Lima/site em dupla/salvar_usuario.php
+++ Sofia Cavalcante e Wanessa Lima/site em dupla/salvar_usuario.php
@@ -1,13 +1,13 @@
 <?php
 if(!isset($_SESSION)) session_start();
+include "cons.php";
+require_once "DLL.php";
 extract($_POST);
 
 $_SESSION['temp_cpf'] = $cpf;
 
-$conteudo = "Nome: $nome\nCPF: $cpf\nEndereco: $endereco\nBairro: $bairro\nCidade: $cidade\nEstado: $estado\nCEP: $cep\n";
-$arq = fopen("usuarios/" . $cpf . ".dat", "w");
-fwrite($arq, $conteudo);
-fclose($arq);
+$consulta = "INSERT INTO usuarios (nome, cpf, endereco, bairro, cidade, estado, cep) VALUES ('$nome', '$cpf', '$endereco', '$bairro', '$cidade', '$estado', '$cep')";
+banco($server, $user, $password, $db, $consulta);
 
 header('Location: cadastro2.php');
 exit;
