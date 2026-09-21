e98c60b1249cfaf58ecfbc83ddea423fb9d60e5a
Sofia Cavalcante <sofiacavalcante011@gmail.com>
2026-09-21 08:37:19 -0300

    Update salvar_login.php

--- Sofia Cavalcante e Wanessa Lima/site em dupla/salvar_login.php
+++ Sofia Cavalcante e Wanessa Lima/site em dupla/salvar_login.php
@@ -1,5 +1,7 @@
 <?php
 if(!isset($_SESSION)) session_start();
+include "cons.php";
+require_once "DLL.php";
 extract($_POST);
 
 if(!isset($_SESSION['temp_cpf'])) { header('Location: cadastro1.php'); exit; }
@@ -7,13 +9,8 @@ $cpf_vinculado = $_SESSION['temp_cpf'];
 
 $senha_cripto = password_hash($senha, PASSWORD_DEFAULT);
 
-$arq = fopen("login/" . $login . ".dat", "w");
-fwrite($arq, $senha_cripto);
-fclose($arq);
-
-$arq_rel = fopen("login/" . $login . "_relacao.dat", "w");
-fwrite($arq_rel, $cpf_vinculado);
-fclose($arq_rel);
+$consulta = "INSERT INTO logins (login, senha, cpf) VALUES ('$login', '$senha_cripto', '$cpf_vinculado')";
+banco($server, $user, $password, $db, $consulta);
 
 unset($_SESSION['temp_cpf']);
 header('Location: login.php');
